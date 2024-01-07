7a02c57c33891606c739fff8e0dcabbdf17f7f13
Zala <zala.developer@gmail.com>
2024-01-07 12:56:04 +1030

    added user update functionality

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers, deleteUserById } from "../db/users";
+import { getUsers, deleteUserById, getUserById } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -24,6 +24,28 @@ export const deleteUser = async (
     const deletedUser = await deleteUserById(id);
 
     return res.json(deletedUser);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+    user.username = username;
+    await user.save();
+    return res.status(200).json(user).end();
   } catch (error) {
     console.error(error);
     return res.sendStatus(400);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { isAuthenticated, isOwner } from "../middleware";
-import { deleteUser, getAllUsers } from "../controllers/users";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
   router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
+  router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
 };
