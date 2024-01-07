84ac7456e13f9bce93ef4d65266952254809c395
Zala <zala.developer@gmail.com>
2024-01-07 10:44:04 +1030

    created middleware for authentication

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import { login, register } from '../controllers/authentication';
+import express from "express";
+import { login, register } from "../controllers/authentication";
 
 export default (router: express.Router) => {
-    router.post('/auth/register', register)
-    router.post('/auth/login', login)
-}
+  router.post("/auth/register", register);
+  router.post("/auth/login", login);
+};
